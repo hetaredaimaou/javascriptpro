95b35fbe4db114e469a0bf3ead7480f41dc1e842
hetaredaimaou <hanadekayu@icloud.com>
2024-03-05 21:12:37 +0900

    aaaa

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log("aaa\n");
+//console.log("aaa\n");
+const button=document.getElementById("button");
+//console.log(button);
+button.addEventListener("click",function () {
+    console.log("aaa\n");
+    
+    console.log(textbox.value);
+    const ele=document.createElement("div");
+    ele.textContent=textbox.value;
+    document.getElementById("list").appendChild(ele);
+})
+
+const textbox=document.getElementById("textbox");
+textbox.addEventListener("change",function(e){
+    console.log(e.target.value);
+})
